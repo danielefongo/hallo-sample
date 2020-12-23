3282162b79b30d351532e9c6d126b8ffcc54e05c
danielefongo <danielefongo@gmail.com>
2020-12-23 21:58:48 +0100

    add more ice servers

--- public/js/client.js
+++ public/js/client.js
@@ -2,7 +2,13 @@ import '../css/style.scss';
 import HalloClient from 'hallo-client'
 
 const iceServers = {
-  iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
+  iceServers: [
+    { urls: 'stun:stun.l.google.com:19302' },
+    { urls: 'stun:stun1.l.google.com:19302' },
+    { urls: 'stun:stun2.l.google.com:19302' },
+    { urls: 'stun:stun3.l.google.com:19302' },
+    { urls: 'stun:stun4.l.google.com:19302' }
+  ]
 }
 
 const hallo = new HalloClient(iceServers)
